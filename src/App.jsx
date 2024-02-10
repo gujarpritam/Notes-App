14043c0923809b2f2724956572c3d97d11f3dccd
Pritam <pritamgujar27@gmail.com>
2024-02-10 19:35:53 +0530

    Added Notes App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NotesApp from "./pages/NotesApp/NotesApp";
+
+function App() {
+  return (
+    <>
+      <NotesApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/NotesApp/NotesApp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotesApp() {
+  return <div>NotesApp</div>;
+}
+
+export default NotesApp;
